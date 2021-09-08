d3bcb46bcf9608ff7a879222d7a4d436a69e5c24
grewallovepreet <90358578+grewallovepreet@users.noreply.github.com>
2021-09-08 19:08:37 -0400

    Update findBook.py

--- findBook.py
+++ findBook.py
@@ -1,1 +1,10 @@
-
+#Check if the book exists in your collection
+collectionOfBooks =  ["Mahan Kosh", "War at the top of the world", "Sherlock Holmes" ]
+print("Enter the name of book: ")
+bookToBeChecked = input()
+for book in collectionOfBooks:
+  if book == bookToBeChecked:
+    print("Yes, I do have the book!")
+    break
+else:
+  print("No, I don't have the book!")
