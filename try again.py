1f11f7b034e7d137b63c58579faaa4b88a2b79de
grewallovepreet <90358578+grewallovepreet@users.noreply.github.com>
2021-09-09 00:51:27 -0400

    Add files via upload

---
+++ try again.py
@@ -0,0 +1,7 @@
+Success = 1
+while Success:
+    print("Winners Never Quit, Quitters Never Win!")
+    if Success == 100:
+        print("Don't Stop! Compete with yourself")
+        break
+    Success = Success + 1
